a014a16ce04649c9d7413f6addbdcaee3814cf19
dadigavasavipriya <112770492+dadigavasavipriya@users.noreply.github.com>
2023-12-08 10:11:06 +0530

    Update Decryption.java

--- Decryption.java
+++ Decryption.java
@@ -4,30 +4,30 @@ public class Decryption{
         String S="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String s="abcdefghijklmnopqrstuvwxyz";
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the Input text");
-        String inputtext=sc.nextLine();
+        System.out.println("Enter the Cipher text");
+        String ciphertext=sc.nextLine();
         System.out.println("Enter the secret key");
         int key=sc.nextInt();
-        String ciphertext="";
-        for(int i=0;i<inputtext.length();i++){
-            char a=inputtext.charAt(i);
+        String plaintext="";
+        for(int i=0;i<ciphertext.length();i++){
+            char a=ciphertext.charAt(i);
             int b=0;
             if(Character.isUpperCase(a)){
                 b=(S.indexOf(a)-key)%26;
                 if(b<0)
-                ciphertext+=S.charAt(b+26);
+                plaintext+=S.charAt(b+26);
                 else
-                 ciphertext+=S.charAt(b);
+                 plaintext+=S.charAt(b);
             }
             else{
                 b=(s.indexOf(a)-key)%26;
                 if(b<0)
-                ciphertext+=s.charAt(b+26);
+                plaintext+=s.charAt(b+26);
                 else
-                 ciphertext+=s.charAt(b);
+                 plaintext+=s.charAt(b);
             }
         }
-        System.out.println("Cipher text is "+ciphertext);
+        System.out.println("Plain text is "+plaintext);
     }
 }
      
